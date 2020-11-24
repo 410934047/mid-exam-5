f9466a8aa6aa8d7e57d2b65f980f1adac7c7cb7f
410934047 <71689110+410934047@users.noreply.github.com>
2020-11-24 15:58:52 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ input.onGesture(Gesture.Shake, function () {
     sprite = randint(1, 6)
     basic.showNumber(sprite)
     if (sprite > 3) {
-        basic.showString("WIN")
+        basic.showString("Win")
     } else {
-        basic.showString("LOSE")
+        basic.showString("Lose")
         game.gameOver()
     }
 })
